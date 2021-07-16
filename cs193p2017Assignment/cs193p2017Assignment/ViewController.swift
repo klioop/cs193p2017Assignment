889459ed70557971c56267c485619ed466cfbe02
Sam <klioop2@gmail.com>
2021-07-16 14:47:28 +0900

    first setting

---
+++ cs193p2017Assignment/cs193p2017Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cs193p2017Assignment
+//
+//  Created by klioop on 2021/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
